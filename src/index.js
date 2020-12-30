d4398e9a07d53b841c48e1e97d9426d0e407494f
MaDud <martaduda@interia.eu>
2020-12-30 20:27:42 +0100

    Add first check for js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,10 @@ const SW_API_BASE_URL = process.env.SW_API_BASE_URL || "https://swapi.dev/api";
 const QUIZ_MAX_TIME = process.env.QUIZ_MAX_TIME_SECONDS ? process.env.QUIZ_MAX_TIME_SECONDS * ONE_SECOND_MILLIS : 120 * ONE_SECOND_MILLIS;
 
 window.onload = () => App({options: {swApiBaseUrl: SW_API_BASE_URL, quizMaxTime: QUIZ_MAX_TIME}})
+
+//toogle menu
+const menuBtn = document.querySelector('.navigation__btn');
+
+console.log(menuBtn)
+
+menuBtn.addEventListener('click', e => console.log('Clicked'))
