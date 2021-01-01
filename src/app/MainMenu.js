830bf44d92e0dbac444efbe8cfc9e40d67cf6d88
MaDud <martaduda@interia.eu>
2021-01-01 20:30:57 +0100

    Create html structure for main menu in JS

--- src/app/MainMenu.js
+++ src/app/MainMenu.js
@@ -1,22 +1,3 @@
-//function to create element with classes
-// constaddElement = (type, ...classes) => {
-//     return document.createElement(type).classList.add(classes.join(' '))
-// }
-
-var my_div = null;
-var newDiv = null;
-
-function addElement()
-{
-  // tworzy nowy element div
-  // i daje jego zawartość
-  newDiv = document.createElement("div");
-  newDiv.innerHTML = "<h1>Hi there and greetings!</h1>";
-
-  // add the newly created element and it's content into the DOM
-  my_div = document.getElementById("org_div1");
-  document.body.insertBefore(newDiv, my_div);
-}
 
 // <!-- <menu class="menu">   
 // <div class="logo">CookBook</div>
@@ -32,27 +13,75 @@ function addElement()
 //             <li class="navigation__listItem">Calculator</li>
 //             <li class="navigation__listItem">Shopping list</li>
 //         </ul>
-//         <div class="navigation__search">
+//     </div>
+//     <div class="navigation__search">
 //             <input type="text" class="navigation__searchInput" placeholder="Search...">
 //             <button class="navigation__searchBtn">
 //                 <i class="fas fa-search"></i>
 //             </button>
-//         </div>
 //     </div>
 // </nav>
 // </menu> -->
 
+const BREAKING_POINT = 1000;
+
+//append multi children to element
+const appendChildrenToElement = (element, ...children) => {
+  for (let child in children) {
+    element.appendChild(children[child])
+  }
+  return element
+}
+
+//create element and his childrens
+const createElementWithChildren = (element, ...children) => {
+  const mainElement = document.createElement(element);
+
+  for (let child in children) {
+    const childElement = document.createElement(children[child]);
+    mainElement.appendChild(childElement)
+  }
+
+  return element
+}
 
 
 export const MainMenu = () => {
 
-    //create menu
-    // const menu = createEl('menu', 'menu');
-    // const logo = createEl('div', 'logo');
-    // const naviagtionList = createEl('ul', 'navigation_list');
-    // document.body.innerHtml = logo;
+    //create menu structure
+    const menu = document.createElement('nav');
 
-   addElement()
+    const logo = document.createElement('div');
+    logo.classList.add('logo');
+    logo.innerText = 'CookBook';
+
+    const menuBtn = document.createElement('button');
+    menuBtn.innerText = 'Click';
+    menuBtn.classList.add('navigation__btn')
+
+    const navList = document.createElement('ul');
+    navList.classList.add('navigation__list')
+    const listElements = ['Main Page', 'Random Receip', 'Nutrition game', 'Calculator', 'Shopping list' ];
+    for (let listItem in listElements) {
+      const listElement = document.createElement('li');
+      listElement.innerText = listElements[listItem];
+      listElement.classList.add('navigation__listItem')
+      navList.appendChild(listElement)
+    }
+
+    const search = document.createElement('form');
+    search.classList.add('search')
+    const searchInput = document.createElement('input');
+    searchInput.classList.add('search__input')
+    const searchBtn = document.createElement('button');
+    searchBtn.classList.add('search__button')
+    appendChildrenToElement(search, searchInput, searchBtn)
+
+    appendChildrenToElement(menu, logo, menuBtn, navList, search)
+
+    //append menu to page 
+    const placeToAppend = document.getElementById('swquiz-app');
+    document.body.insertBefore(menu, placeToAppend)
 
     // //toogle menu
     // const menuBtn = document.querySelector('.navigation__btn');
