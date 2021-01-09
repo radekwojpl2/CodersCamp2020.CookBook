0502649b8adde5242b8bd4532a82e8d09d7ac7f2
mateugazi <mateugazi182@outlook.com>
2021-01-09 23:40:37 +0100

    Added clear button

--- calculator.js
+++ calculator.js
@@ -1,17 +1,22 @@
+const outputDiv = document.querySelector('.calculator__form__output');
+
 function calculate() {
     const sourceAmount = parseInt(document.querySelector('.calculator__form__input').value);
     const sourceUnit = document.querySelector('.calculator__form__from').value;
     const targetUnit = document.querySelector('.calculator__form__to').value;
-    const outputDiv = document.querySelector('.calculator__form__output');
     const apiKey = '7c1cea1c1c664db5a38edbf2dd21484e'
 
     fetch(`https://api.spoonacular.com/recipes/convert?ingredientName=this&sourceAmount=${sourceAmount}&sourceUnit=${sourceUnit}&targetUnit=${targetUnit}&apiKey=${apiKey}`)
         .then(res => res.json())
-        .then(data => outputDiv.innerHTML = data.answer)
+        .then(data => outputDiv.innerHTML = data.answer);
 }
 
-calcButton = document.querySelector('.calculator__form__button')
-calcButton.addEventListener('click', calculate, false)
+const calcButton = document.querySelector('.calculator__form__calculate');
+const clearButton = document.querySelector('.calculator__form__clear');
+calcButton.addEventListener('click', calculate, false);
+clearButton.addEventListener('click', () => {
+    outputDiv.innerHTML = 'Answer: '
+})
 
 console.log('xd')
 //7c1cea1c1c664db5a38edbf2dd21484e
