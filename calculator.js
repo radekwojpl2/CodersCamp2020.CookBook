8ec87c276f69d066b9a5e895c052770e235e0c3b
mateugazi <mateugazi182@outlook.com>
2021-01-09 23:36:44 +0100

    Added fetch to js and select inputs to html

--- calculator.js
+++ calculator.js
@@ -1,8 +1,13 @@
 function calculate() {
-    const inputNumber = parseInt(document.querySelector('.calculator__form__input').value);
-    const outputNumber = document.querySelector('.calculator__form__output')
+    const sourceAmount = parseInt(document.querySelector('.calculator__form__input').value);
+    const sourceUnit = document.querySelector('.calculator__form__from').value;
+    const targetUnit = document.querySelector('.calculator__form__to').value;
+    const outputDiv = document.querySelector('.calculator__form__output');
+    const apiKey = '7c1cea1c1c664db5a38edbf2dd21484e'
 
-    outputNumber.innerHTML = inputNumber;
+    fetch(`https://api.spoonacular.com/recipes/convert?ingredientName=this&sourceAmount=${sourceAmount}&sourceUnit=${sourceUnit}&targetUnit=${targetUnit}&apiKey=${apiKey}`)
+        .then(res => res.json())
+        .then(data => outputDiv.innerHTML = data.answer)
 }
 
 calcButton = document.querySelector('.calculator__form__button')
