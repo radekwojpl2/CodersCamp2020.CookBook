3a8d3b0c419034392210577bed8a651660ea475f
mateugazi <mateugazi182@outlook.com>
2021-01-13 14:33:32 +0100

    Fixed some errors with calculate function

--- calculator.js
+++ calculator.js
@@ -1,21 +1,32 @@
 const outputDiv = document.querySelector('.calculator__form__output');
+const apiKey = '7c1cea1c1c664db5a38edbf2dd21484e';
 
 function calculate() {
-    const sourceAmount = parseInt(document.querySelector('.calculator__form__input').value);
+    const sourceAmount = parseFloat(document.querySelector('.calculator__form__input').value);
     const sourceUnit = document.querySelector('.calculator__form__from').value;
     const targetUnit = document.querySelector('.calculator__form__to').value;
-    const apiKey = '7c1cea1c1c664db5a38edbf2dd21484e';
 
-    fetch(`https://api.spoonacular.com/recipes/convert?ingredientName=this&sourceAmount=${sourceAmount}&sourceUnit=${sourceUnit}&targetUnit=${targetUnit}&apiKey=${apiKey}`)
-        .then(res => res.json())
-        .then(data => outputDiv.innerHTML = data.answer);
+    if (isNaN(sourceAmount)) {
+        outputDiv.innerHTML = 'Please enter the correct number';
+    } else {
+        fetch(`https://api.spoonacular.com/recipes/convert?ingredientName=this&sourceAmount=${sourceAmount}&sourceUnit=${sourceUnit}&targetUnit=${targetUnit}&apiKey=${apiKey}`)
+            .then(res => res.json())
+            .then(data => {
+                data.answer !== undefined ?
+                    outputDiv.innerHTML = data.answer :
+                    outputDiv.innerHTML = 'Sorry, something went wrong, please try again'
+            })
+            .catch(() => {
+                outputDiv.innerHTML = 'Sorry, something went wrong, please try again';
+            })
+    }
 }
 
 const calcButton = document.querySelector('.calculator__form__calculate');
 const clearButton = document.querySelector('.calculator__form__clear');
 calcButton.addEventListener('click', calculate, false);
 clearButton.addEventListener('click', () => {
-    outputDiv.innerHTML = 'Answer: ';
+    outputDiv.innerHTML = 'answer goes here';
 })
 
 console.log('xd')
