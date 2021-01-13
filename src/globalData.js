0a815a10a3e20a748b2dcc32c5d832a998362db8
MaDud <martaduda@interia.eu>
2021-01-13 19:45:10 +0100

    Create file with global data

---
+++ src/globalData.js
@@ -0,0 +1,5 @@
+export const API_KEY = 'a69c65ede3bb4ac3b262c5b425b4f835';
+
+export const API = {
+    getRecipeInformation: (id) => `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`,
+}
