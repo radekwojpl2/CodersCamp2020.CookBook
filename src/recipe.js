896ebfa5768c27eb2ff3620e4d79c273c6101f54
MaDud <martaduda@interia.eu>
2021-01-13 19:56:50 +0100

    Receive data for recipe

--- src/recipe.js
+++ src/recipe.js
@@ -1,3 +1,25 @@
 import {MainMenu} from './app/MainMenu.js'; 
+import {API} from './globalData.js';
 
-MainMenu()
+//create navigation bar
+MainMenu();
+
+//function to get id from url
+const getParams = () => {
+    const param = window.location.href.slice(window.location.href.indexOf('?') +1).split('=');
+    return {'id':param[1]}
+}
+
+const param = getParams();
+
+//function to get data from spoon
+fetch(API.getRecipeInformation(param.id))
+.then(response => {
+    if (response.ok) {
+        return response.json()
+    } 
+    throw new Error ("Ups... We didn't receive any data for this recipe")
+})
+.then(recipe => console.log(recipe))
+.catch(error => console.log(error))
+
