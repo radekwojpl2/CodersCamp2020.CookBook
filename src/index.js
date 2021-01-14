b391f7fda52f9a65ba002aa9aa9ed991aef77de2
MaDud <martaduda@interia.eu>
2021-01-14 23:10:37 +0100

    Come back to original settings for index.html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'regenerator-runtime/runtime'; //async/await with Parcel
 import {App} from "./app/App";
-import {MainMenu} from './app/MainMenu.js';
-import {MENU} from './GlobalData.js';
+
 
 const ONE_SECOND_MILLIS = 1000;
 const SW_API_BASE_URL = process.env.SW_API_BASE_URL || "https://swapi.dev/api";
@@ -9,6 +8,5 @@ const QUIZ_MAX_TIME = process.env.QUIZ_MAX_TIME_SECONDS ? process.env.QUIZ_MAX_T
 
 window.onload = () => App({options: {swApiBaseUrl: SW_API_BASE_URL, quizMaxTime: QUIZ_MAX_TIME}});
 
-MainMenu(MENU.mainPage)
 
 
