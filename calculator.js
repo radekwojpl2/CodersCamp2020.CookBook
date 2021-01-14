94958895c4490e010d3f6d79b16f0946b059cea1
mateugazi <mateugazi182@outlook.com>
2021-01-14 11:52:36 +0100

    Changed the calculate function, in the way that it can take parameters

--- calculator.js
+++ calculator.js
@@ -1,11 +1,7 @@
 const outputDiv = document.querySelector('.calculator__form__output');
 const apiKey = '7c1cea1c1c664db5a38edbf2dd21484e';
 
-function calculate() {
-    const sourceAmount = parseFloat(document.querySelector('.calculator__form__input').value);
-    const sourceUnit = document.querySelector('.calculator__form__from').value;
-    const targetUnit = document.querySelector('.calculator__form__to').value;
-
+function calculate(sourceAmount, sourceUnit, targetUnit, outputDiv) {
     if (isNaN(sourceAmount)) {
         outputDiv.innerHTML = 'Please enter the correct number';
     } else {
@@ -24,7 +20,20 @@ function calculate() {
 
 const calcButton = document.querySelector('.calculator__form__calculate');
 const clearButton = document.querySelector('.calculator__form__clear');
-calcButton.addEventListener('click', calculate, false);
+const fromSelect = document.querySelector('.calculator__form__from');
+const toSelect = document.querySelector('.calculator__form__to');
+let fromValue = fromSelect.value;
+let toValue = toSelect.value;
+fromSelect.addEventListener('change', (event) => {
+    fromValue = event.target.value;
+})
+toSelect.addEventListener('change', (event) => {
+    toValue = event.target.value;
+})
+calcButton.addEventListener('click', () => {
+    let sourceValue = parseFloat(document.querySelector('.calculator__form__input').value);
+    calculate(sourceValue, fromValue, toValue, outputDiv);
+});
 clearButton.addEventListener('click', () => {
     outputDiv.innerHTML = 'answer goes here';
 })
