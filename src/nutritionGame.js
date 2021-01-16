eeb1d00c1e88497dbf42bd0d91f0d33e7d6b942b
Kinga Mamak <kingam280@gmail.com>
2021-01-16 08:47:47 +0100

    Add changes after merge

--- src/nutritionGame.js
+++ src/nutritionGame.js
@@ -1,4 +1,7 @@
 import 'regenerator-runtime/runtime';
 import { Game } from './app/Game.js';
+import { MainMenu } from './app/MainMenu.js'
+import { MENU } from './GlobalData.js'
 
+MainMenu(MENU.nutritionGame)
 const nutritionGame = new Game() 
